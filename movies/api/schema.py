dad3c4399ff15e4fab6f531a25f0215efe66d584
Ruban Sahoo <rubansahoo1@gmail.com>
2022-05-16 10:08:41 +0530

    first commit

---
+++ movies/api/schema.py
@@ -0,0 +1,93 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+from .models import Movie, Director
+
+
+class MovieType(DjangoObjectType):
+    class Meta:
+        model = Movie
+
+    movie_age = graphene.String()
+
+    def resolve_movie_age(self, info):
+        return "Old movie" if self.year < 2000 else "New movie"
+
+
+class DirectorType(DjangoObjectType):
+    class Meta:
+        model = Director
+
+
+class Query(graphene.ObjectType):
+    all_movies = graphene.List(MovieType)
+    movie = graphene.Field(MovieType, id=graphene.Int(), title=graphene.String())
+    all_directors = graphene.List(DirectorType)
+
+    def resolve_all_movies(self, info, **kwargs):
+        return Movie.objects.all()
+
+    def resolve_movie(self, info, **kwargs):
+        id = kwargs.get('id')
+        title = kwargs.get('title')
+
+        if id is not None:
+            return Movie.objects.get(pk=id)
+
+        if title is not None:
+            return Movie.objects.get(title=title)
+
+        return None
+
+    def resolve_all_directors(self, info, **kwargs):
+        return Director.objects.all()
+
+
+class MovieCreateMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        year = graphene.Int(required=True)
+
+    movie = graphene.Field(MovieType)
+
+    def mutate(self, info, title, year):
+        movie = Movie.objects.create(title=title, year=year)
+
+        return MovieCreateMutation(movie=movie)
+
+
+class MovieUpdateMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        year = graphene.Int()
+        id = graphene.ID(required=True)
+
+    movie = graphene.Field(MovieType)
+
+    def mutate(self, info, id,  title, year):
+        movie = Movie.objects.get(pk=id)
+        if title is not None:
+            movie.title = title
+        if year is not None:
+            movie.year = year
+        movie.save()
+
+        return MovieUpdateMutation(movie=movie)
+
+
+class MovieDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    movie = graphene.Field(MovieType)
+
+    def mutate(self, info, id):
+        movie = Movie.objects.get(pk=id)
+        movie.delete()
+
+        return MovieDeleteMutation(movie=None)
+
+
+class Mutation:
+    create_movie = MovieCreateMutation.Field()
+    update_movie = MovieUpdateMutation.Field()
+    delete_movie = MovieDeleteMutation.Field()
